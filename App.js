8b904525a0659f707de3c10123daa54c978a8b1b
marcelo <mark.gt.mf@gmail.com>
2021-08-26 00:24:25 -0300

    first commit

--- App.js
+++ App.js
@@ -2,27 +2,40 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View,ImageBackground } from 'react-native';
 
-
- 
-
 export default function App() {
 
-  const image = require ('./resources/');
+  const image = require('./resources/teste.jpg');
 
   return (
     <View style={{flex:1}}>
+
       <ImageBackground source={image} style={styles.image}>
-      <Text>Inside</Text>
+        <View style={styles.coverView}>
+          <Text style={styles.textHeader}>Lista de Tarefas</Text>
+        </View>       
       </ImageBackground>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   image: {
-    flex:1,
-    resizeMode:"cover",
-    justifyContent:"center"
-    
+    width:'100%',
+    height:80,
+    resizeMode:"cover", 
   },
+  coverView:{
+    width:'100%',
+    height:80,
+    backgroundColor:'rgba(0,0,0,0.5)'
+
+  },
+  textHeader:{
+    textAlign:'center',
+    color:'white',
+    fontSize:20,
+    marginTop:20
+    
+  }
 });
